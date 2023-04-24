cf16bce9ea59f02fb9b947c522dad822b838befa
innaslota <innaslota8@gmail.com>
2023-04-24 13:53:03 +0300

    refactor(basic-3.4): remove tag selectors

--- app.js
+++ app.js
@@ -82,8 +82,8 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
-    var label=listItem.querySelector("label");
+    var editInput=listItem.querySelector(".text-input");
+    var label=listItem.querySelector(".label");
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .editmode
